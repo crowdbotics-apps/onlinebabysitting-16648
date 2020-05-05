1bc0c090a23277ea9d2619b4109fc4fff79d6b6d
Crowdbotics <team@crowdbotics.com>
2020-05-05 23:40:20 +0000

    Initial commit.

---
+++ backend/onlinebabysitting_16648/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for onlinebabysitting_16648 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinebabysitting_16648.settings')
+
+application = get_wsgi_application()
